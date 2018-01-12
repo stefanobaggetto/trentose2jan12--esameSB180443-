679fb7e2beb45032bbfa3281a5255b24dd1f1139
stefanobaggetto <stefano.baggetto@studenti.unitn.it>
2018-01-12 10:39:27 +0100

    aggiunto area.js

--- area.js
+++ area.js
@@ -1,21 +1,34 @@
-const express = require('express'),
-    bodyParser = require('body-parser');
+var express = require('express');
+var bodyParser = require('body-parser');
 
 const app = express();
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const nomeModulo = require('./rettangolo')
+
+var port = process.env.PORT || 5000;
+
+var router = express.Router();
 
 
-app.use('/api', getArea)
+router.route('/getArea')
+.get(function (req, res) {
+    var side1 = parseInt(req.query.side1);
+    var side2 = parseInt(req.query.side2);
+    var r = side1 * side2;
+    if (side1>=0&&side2>=0){
+        res.json(r);
+    }
+    else{
+        r=-1;
+    res.json(r);
+    }
+    res.status = 200;
+    });
 
-app.set('port', (process.env.PORT || 5000));
 
-app.listen(app.get('port'), function() {
-    console.log('Node app is running on port', app.get('port'));
-});
-
-// middleware route to support CORS and preflighted requests
 app.use(function (req, res, next) {
     // do logging
     console.log('Something is happening.');
@@ -24,13 +37,15 @@ app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Content-Type', 'application/json');
     if (req.method == 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, DELETE');
         return res.status(200).json({});
     }
-    
     // make sure we go to the next routes
     next();
 });
+
+// register our router on /api
+app.use('/api', router);
 
 // handle invalid requests and internal error
 app.use((req, res, next) => {
@@ -42,3 +57,7 @@ app.use((err, req, res, next) => {
     res.status(err.status || 500);
     res.json({ error: { message: err.message } });
 });
+
+
+app.listen(port);
+console.log('Magic happens on port ' + port);
